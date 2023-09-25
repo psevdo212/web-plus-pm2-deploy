255eb72eb89d27d6accb34c4508938330b7327da
psevdo212 <psevdo-2002@yandex.ru>
2023-09-25 20:29:07 +0300

    2

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -16,6 +16,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': `cd ${DEPLOY_PATH}/source/frontend && npm i && npm run build`,
     },
   },
