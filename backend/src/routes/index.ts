e92ef680bf6169a420454b71de7a851dc2a93303
psevdo212 <psevdo-2002@yandex.ru>
2023-09-26 16:36:37 +0300

    dbadress

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -11,11 +11,17 @@ import {
 import { validateUserBody, validateAuthentication } from '../middlewares/validatons';
 
 const router = Router();
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
 
 // все роуты, кроме /signin и /signup, защищены авторизацией;
 router.use(auth);
+
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
