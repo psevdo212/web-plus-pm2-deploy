20e9ee97a866dbb49fd05b31068ca7bb51a54001
psevdo212 <psevdo-2002@yandex.ru>
2023-09-26 22:38:39 +0300

    API

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,7 +139,7 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('psevdoback.nomoredomainsrocks.ru');
+  const api = new Api('api.psedvofront.nomoredomainsrocks.ru');
   
   export default api;
   
